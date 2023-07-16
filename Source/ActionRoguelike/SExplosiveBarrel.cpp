398a3c34b2501374d1ebde50f00053be7f953681
yutaka-nakanishi <yutaka.nakanishi@mixi.co.jp>
2023-07-16 15:56:22 +0900

    ExplosiveBarrel re-inplemented in C++
    added - ExplosiveBarrel re-inplemented in C++

---
+++ Source/ActionRoguelike/SExplosiveBarrel.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	StaticMeshComponent->OnComponentHit.AddDynamic(this,&ASExplosiveBarrel::OnExplosiveBarrelHit);
+	RootComponent = StaticMeshComponent;
+
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>("RadialForceComponent");
+	RadialForceComponent->SetupAttachment(StaticMeshComponent);
+
+
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+void ASExplosiveBarrel::OnExplosiveBarrelHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComponent->FireImpulse();
+}
+
---
+++ Source/ActionRoguelike/SExplosiveBarrel.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SExplosiveBarrel.generated.h"
+
+class URadialForceComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASExplosiveBarrel();
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	URadialForceComponent* RadialForceComponent;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// UPrimitiveComponent*, HitComponent, AActor*, OtherActor, UPrimitiveComponent*, OtherComp, FVector, NormalImpulse, const FHitResult&, Hit 
+	UFUNCTION()
+	void OnExplosiveBarrelHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
